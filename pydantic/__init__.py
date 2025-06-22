a259e7ff4c639475ea1d7431d7c6f40e78a64b87
Zeus-Eternal <121900625+Zeus-Eternal@users.noreply.github.com>
2025-06-22 15:45:37 -0700

    Move Tauri config and update docs

--- pydantic/__init__.py
+++ pydantic/__init__.py
@@ -1,4 +1,7 @@
+from typing import Any
+
+
 class BaseModel:
-    def __init__(self, **data):
+    def __init__(self, **data: Any) -> None:
         for k, v in data.items():
             setattr(self, k, v)
