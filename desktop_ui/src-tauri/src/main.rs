b246548b18ea462dd4501d4097966bdae64748e3
Zeus-Eternal <zeus.the.eternal@gmail.com>
2025-06-30 23:17:04 -0400

    .

--- desktop_ui/src-tauri/src/main.rs
+++ desktop_ui/src-tauri/src/main.rs
@@ -1,6 +1,14 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri_plugin_http::Http;
+use tauri_plugin_log::Builder as LogBuilder;
+use tauri_plugin_shell::Shell;
+
 fn main() {
-  app_lib::run();
+    tauri::Builder::default()
+        .plugin(Http::default())
+        .plugin(Shell::default())
+        .plugin(LogBuilder::default().build())
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
