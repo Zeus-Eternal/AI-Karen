868d42e41aafd589697c917fee1612135126b18b
Zeus-Eternal <121900625+Zeus-Eternal@users.noreply.github.com>
2025-08-10 15:20:57 -0400

    Add package initializers for copilotkit and middleware

---
+++ src/ai_karen_engine/copilotkit/__init__.py
@@ -0,0 +1,8 @@
+"""Integration utilities for CopilotKit."""
+
+from ai_karen_engine.copilotkit.error_handler import (
+    CopilotKitErrorType,
+    CopilotKitFallbackHandler,
+)
+
+__all__ = ["CopilotKitErrorType", "CopilotKitFallbackHandler"]
